c44f0f558864b27bea5ad2b436ef0537a27b62e4
raelaraujo0 <raellyedson1@gmail.com>
2023-12-28 22:58:16 -0300

    inicio roupas/clientes UPDATE

--- projeto/projeto/urls.py
+++ projeto/projeto/urls.py
@@ -14,10 +14,12 @@ urlpatterns = [
     ## CLIENTES
     path('cliente/', views.criar_cliente, name='criar_cliente'),
     path('excluircliente/', views.excluir_cliente, name='excluir_cliente'),
+    path('atualizarcliente/', views.atualizar_cliente, name='atualizar_cliente'),
 
     ## ROUPAS
     path('roupa/', views.criar_roupa, name='criar_roupa'),
     path('excluirroupa/', views.excluir_roupa, name='excluir_roupa'),
+    path('atualizarroupa/', views.atualizar_roupa, name='atualizar_roupa'),
 
     path('listagem/', views.listartudo, name='listartudo'),
 ]
--- projeto/projetoapp/views.py
+++ projeto/projetoapp/views.py
@@ -35,6 +35,21 @@ def excluir_cliente(request):
     
     return render(request, 'brecho/cliente/excluircliente.html')
 
+def atualizar_cliente(request):
+    cliente = None
+    if request.method == 'POST':
+        idcliente = request.POST.get('idcliente')
+        cliente = get_object_or_404(Cliente, pk=idcliente)
+        form = Cliente(request.POST, instance=cliente)
+        if form.is_valid():
+            form.save()
+            return HttpResponse({'message': 'Cliente atualizado'})
+        return redirect('atualizar_cliente')
+    else:
+        form = ClienteForm(instance=cliente)
+
+    return render(request, 'brecho/cliente/atualizarcliente.html', {'form': form, 'cliente': cliente })  
+
 ## DIVISAO ENTRE CLIENTES E ROUPAS
 
 def criar_roupa(request):
@@ -56,6 +71,21 @@ def excluir_roupa(request):
         return HttpResponse({'message': 'Roupa excluida'})
     
     return render(request, 'brecho/roupa/excluirroupa.html')
+
+def atualizar_roupa(request):
+    roupa = None
+    if request.method == 'POST':
+        idroupa = request.POST.get('idroupa')
+        roupa = get_object_or_404(Roupa, pk=idroupa)
+        form = Roupa(request.POST, instance=roupa)
+        if form.is_valid():
+            form.save()
+            return redirect('atualizar_roupa')
+        return HttpResponse({'message': 'Roupa atualizada'})
+    else:
+        form = RoupaForm(instance=roupa)
+
+    return render(request, 'brecho/roupa/atualizarroupa.html', {'form': form, 'roupa': roupa})
 
 ######################
 
