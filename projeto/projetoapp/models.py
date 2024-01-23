b18f1a33a58ea9823e2d715ccc8a622f35a4b413
raelaraujo0 <raellyedson1@gmail.com>
2024-01-23 09:05:45 -0300

    models

--- projeto/projetoapp/models.py
+++ projeto/projetoapp/models.py
@@ -11,6 +11,7 @@ class Cliente(models.Model):
 
 class Roupa(models.Model):
     idroupa = models.AutoField(primary_key = True)
+    nomebrecho = models.CharField(max_length = 20)
     Tamanho = models.CharField(max_length=4)
     Preco = models.DecimalField(max_digits =10, decimal_places=2, null=True, validators=[MinValueValidator(0)])
     Categoria = models.CharField(max_length = 20, null = True)
@@ -22,8 +23,8 @@ class Roupa(models.Model):
 
 class Venda(models.Model):
     idvenda = models.AutoField(primary_key= True)
-    roupa_comprada = models.ManyToManyField('Roupa', blank = True)
-    cliente_comprou = models.ManyToManyField('Cliente', blank= True)
+    roupa_comprada = models.ForeignKey(Cliente, on_delete=models.SET_NULL, null=True)
+    cliente_comprou = models.ForeignKey(Roupa, on_delete=models.SET_NULL, null=True)
     DataVenda = models.DateField(null=True)
 
     def __str__(self):
